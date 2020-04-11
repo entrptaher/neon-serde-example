553e5b801aea070a038ff8f5992883149a4a67f3
Taher <entrptaher@gmail.com>
2020-04-11 17:55:25 +0600

    add buffer example

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,134 +1,57 @@
+extern crate neon;
 extern crate serde;
 extern crate serde_json;
 
-use std::fmt;
-
-use neon::prelude::*;
-use neon_serde;
-use serde::de::{Deserializer, Visitor};
-
 #[macro_use]
 extern crate serde_derive;
-use serde::Deserialize;
 
-#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
-struct Data {
-    name: Name,
+#[macro_use]
+extern crate neon_serde;
+
+use neon::prelude::*;
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize)]
+struct Link {
+    page: String,
+    group: String,
+    name: String,
     index: usize,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Link {
-    page: Page,
-    group: Group,
-    stuff: Data,
+#[derive(Serialize, Deserialize)]
+struct NamedIndex {
+    pub name: String,
+    pub index: usize,
 }
+fn string(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let input: String = cx.argument::<JsString>(0)?.value();
 
-#[derive(Debug, Serialize, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
-struct Name(usize);
+    // let b: Handle<JsBuffer> = cx.argument(0)?;
 
-#[derive(Debug, Serialize, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
-struct Group(usize);
+    // let buffer = cx.borrow(&b, |data| {
+    //     return data.as_slice::<u8>();
+    // });
 
-#[derive(Debug, Serialize, PartialOrd, Ord, PartialEq, Eq, Clone, Copy)]
-struct Page(usize);
+    let mut data: HashMap<String, HashMap<String, Vec<NamedIndex>>> =
+        serde_json::from_str(&input).unwrap();
 
-struct GroupVisitor;
+    let mut list: Vec<Link> = Vec::with_capacity(512);
 
-impl<'de> Visitor<'de> for GroupVisitor {
-    type Value = Group;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("an group name")
+    for (page, mut groups) in data.drain() {
+        for (group, mut named_indexes) in groups.drain() {
+            for NamedIndex { name, index } in named_indexes.drain(..) {
+                let page = page.clone();
+                let group = group.clone();
+                list.push(Link {
+                    page,
+                    group,
+                    name,
+                    index,
+                });
+            }
+        }
     }
-
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
-        Ok(Group(value[5..].parse().unwrap()))
-    }
-}
-
-impl<'de> Deserialize<'de> for Group {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(GroupVisitor)
-    }
-}
-
-struct PageVisitor;
-
-impl<'de> Visitor<'de> for PageVisitor {
-    type Value = Page;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("an page name")
-    }
-
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
-        Ok(Page(value[4..].parse().unwrap()))
-    }
-}
-
-impl<'de> Deserialize<'de> for Page {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(PageVisitor)
-    }
-}
-
-struct NameVisitor;
-
-impl<'de> Visitor<'de> for NameVisitor {
-    type Value = Name;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("an name")
-    }
-
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
-        Ok(Name(value[4..].parse().unwrap()))
-    }
-}
-
-impl<'de> Deserialize<'de> for Name {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(NameVisitor)
-    }
-}
-
-#[derive(Deserialize)]
-struct ObjectTuple<K, V>(#[serde(with = "tuple_vec_map")] Vec<(K, V)>)
-where
-    K: serde::de::DeserializeOwned,
-    V: serde::de::DeserializeOwned;
-
-fn hello(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let arg0: String = cx.argument::<JsString>(0)?.value();
-
-    let object: ObjectTuple<Page, ObjectTuple<Group, Vec<Data>>> =
-        serde_json::from_str(&arg0).unwrap();
-
-    let list: Vec<Link> = object
-        .0
-        .iter()
-        .map(|(page_key, page)| {
-            page.0.iter().map(move |(group_key, group)| {
-                group.iter().map(move |data| Link {
-                    stuff: *data,
-                    page: *page_key,
-                    group: *group_key,
-                })
-            })
-        })
-        .flatten()
-        .flatten()
-        .collect();
 
     // return back to nodejs
     let result = neon_serde::to_value(&mut cx, &list)?;
@@ -136,4 +59,41 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsValue> {
     Ok(result)
 }
 
-register_module!(mut cx, { cx.export_function("hello", hello) });
+fn buffer(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let b: Handle<JsBuffer> = cx.argument(0)?;
+
+    let buffer = cx.borrow(&b, |data| {
+        return data.as_slice::<u8>();
+    });
+
+    let mut data: HashMap<String, HashMap<String, Vec<NamedIndex>>> =
+        serde_json::from_slice(&buffer).unwrap();
+
+    let mut list: Vec<Link> = Vec::with_capacity(512);
+
+    for (page, mut groups) in data.drain() {
+        for (group, mut named_indexes) in groups.drain() {
+            for NamedIndex { name, index } in named_indexes.drain(..) {
+                let page = page.clone();
+                let group = group.clone();
+                list.push(Link {
+                    page,
+                    group,
+                    name,
+                    index,
+                });
+            }
+        }
+    }
+
+    // return back to nodejs
+    let result = neon_serde::to_value(&mut cx, &list)?;
+
+    Ok(result)
+}
+
+register_module!(mut cx, {
+    cx.export_function("string", string);
+    cx.export_function("buffer", buffer);
+    Ok(())
+});
