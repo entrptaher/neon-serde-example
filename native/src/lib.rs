beca635b6489aac5f40edb1e9b4cc2702f4c0b7a
Taher <entrptaher@gmail.com>
2020-04-13 05:08:19 +0600

    add rayon and hashbrown

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,16 +1,19 @@
-extern crate neon;
+// SERDE
 extern crate serde;
 extern crate serde_json;
 
 #[macro_use]
 extern crate serde_derive;
 
-#[macro_use]
-extern crate neon_serde;
-
+// NEON
+extern crate neon;
 use neon::prelude::*;
-use std::collections::HashMap;
 
+// STD
+use rayon::prelude::*;
+use hashbrown::HashMap;
+
+// STRUCTS
 #[derive(Serialize, Deserialize)]
 struct Link {
     page: String,
@@ -24,79 +27,17 @@ struct NamedIndex {
     pub name: String,
     pub index: usize,
 }
-fn string(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let input: String = cx.argument::<JsString>(0)?.value();
 
-    let mut data: HashMap<String, HashMap<String, Vec<NamedIndex>>> =
-        serde_json::from_str(&input).unwrap();
-
-    let mut list: Vec<Link> = Vec::with_capacity(512);
-
-    for (page, mut groups) in data.drain() {
-        for (group, mut named_indexes) in groups.drain() {
-            for NamedIndex { name, index } in named_indexes.drain(..) {
-                let page = page.clone();
-                let group = group.clone();
-                list.push(Link {
-                    page,
-                    group,
-                    name,
-                    index,
-                });
-            }
-        }
-    }
-
-    // return back to nodejs
-    let result = neon_serde::to_value(&mut cx, &list)?;
-
-    Ok(result)
+struct BackgroundTask<'a> {
+    buf: &'a [u8],
 }
 
-fn buffer(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let b: Handle<JsBuffer> = cx.argument(0)?;
-
-    let buffer = cx.borrow(&b, |data| {
-        return data.as_slice::<u8>();
-    });
-
-    let mut data: HashMap<String, HashMap<String, Vec<NamedIndex>>> =
-        serde_json::from_slice(&buffer).unwrap();
-
-    let mut list: Vec<Link> = Vec::with_capacity(512);
-
-    for (page, mut groups) in data.drain() {
-        for (group, mut named_indexes) in groups.drain() {
-            for NamedIndex { name, index } in named_indexes.drain(..) {
-                let page = page.clone();
-                let group = group.clone();
-                list.push(Link {
-                    page,
-                    group,
-                    name,
-                    index,
-                });
-            }
-        }
-    }
-
-    // return back to nodejs
-    let result = neon_serde::to_value(&mut cx, &list)?;
-
-    Ok(result)
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct BackgroundTask {
-    buf: Vec<u8>,
-    // extra
-}
-
+// FN
 fn direct(mut cx: FunctionContext) -> JsResult<JsString> {
     let buf = cx.argument::<JsBuffer>(0)?;
     let buf = cx.borrow(&buf, |data| data.as_slice().to_vec());
-    let task = BackgroundTask { buf };
-    // println!("{:#?}",task);
+    let task = BackgroundTask { buf: &buf };
+
     let mut data: HashMap<String, HashMap<String, Vec<NamedIndex>>> = serde_json::from_slice(&task.buf).unwrap();
     let mut list: Vec<Link> = Vec::with_capacity(512);
     
@@ -117,39 +58,9 @@ fn direct(mut cx: FunctionContext) -> JsResult<JsString> {
     
     let str_data = serde_json::to_string(&list).unwrap();
     Ok(cx.string(str_data))
-
-    // let js_value = neon_serde::to_value(&mut cx, &list)?;
-    // Ok(js_value)
 }
 
 register_module!(mut cx, {
     cx.export_function("direct", direct);
-    cx.export_function("string", string);
-    cx.export_function("buffer", buffer);
     Ok(())
 });
-
-// extern crate serde_bytes;
-// export! {
-//     fn direct(input: serde_bytes::ByteBuf) -> Vec<Link> {
-//         // let buf = input.as_slice().to_vec();
-//         // let task = BackgroundTask { buf };
-//         let mut data: HashMap<String, HashMap<String, Vec<NamedIndex>>> = serde_json::from_slice(&input).unwrap();
-//         let mut list: Vec<Link> = Vec::with_capacity(512);
-//         for (page, mut groups) in data.drain() {
-//             for (group, mut named_indexes) in groups.drain() {
-//                 for NamedIndex { name, index } in named_indexes.drain(..) {
-//                     let page = page.clone();
-//                     let group = group.clone();
-//                     list.push(Link {
-//                         page,
-//                         group,
-//                         name,
-//                         index,
-//                     });
-//                 }
-//             }
-//         }
-//         list
-//     }
-// }
