59ab2ae880e9239197d48f4b12705e3757200755
Taher <entrptaher@gmail.com>
2020-04-14 08:33:33 +0600

    fib async

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -8,7 +8,45 @@ use neon::prelude::*;
 pub mod hashmap;
 pub mod objecttuple;
 
+struct BackgroundTask {
+    argument: usize,
+}
+
+fn fib(n: u64) -> u64 {
+    if n <= 1 {
+        return 1;
+    }
+    fib(n - 1) + fib(n - 2)
+}
+
+impl Task for BackgroundTask {
+    type Output = usize;
+    type Error = String;
+    type JsEvent = JsNumber;
+    fn perform(&self) -> Result<Self::Output, Self::Error> {
+        let num = self.argument;
+        Ok(fib(num as u64) as usize)
+    }
+    fn complete(
+        self,
+        mut cx: TaskContext,
+        result: Result<Self::Output, Self::Error>,
+    ) -> JsResult<Self::JsEvent> {
+        Ok(cx.number(result.unwrap() as f64))
+    }
+}
+
+pub fn perform_async_task(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    let n = cx.argument::<JsNumber>(0)?.value() as usize;
+    let cb = cx.argument::<JsFunction>(1)?;
+    let task = BackgroundTask { argument: n };
+    task.schedule(cb);
+    Ok(cx.undefined())
+}
+
 register_module!(mut cx, {
+    cx.export_function("perform_async_task", perform_async_task);
+
     cx.export_function("hashmap_buffer_neon_value", hashmap::buffer_neon_value)
         .expect("export function");
     cx.export_function("hashmap_buffer_serde_string", hashmap::buffer_serde_string)
